d91d4a8fe113ecbe5c48e7dc2d2b88cc60dd37d5
darrpama <darrpama@darrpamas-MacBook-Air.local>
2024-02-07 14:04:47 +0300

    init comit

---
+++ src/d00_ex00/Program.cs
@@ -0,0 +1,8 @@
+﻿// Первая строка, которая является комментарием
+
+/*
+    Многострочный комментарий
+*/
+Console.WriteLine("Hello, World!");  // Выводим Hello, world!
+Console.WriteLine("AbobA");          // Выводим абобу
+
