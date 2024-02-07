4bfa1e8d8b259ea52520ef9c1f76d490be09ad49
darrpama <darrpama@darrpamas-MacBook-Air.local>
2024-02-07 14:39:54 +0300

    example of using arguments of program

--- src/d00_ex00/Program.cs
+++ src/d00_ex00/Program.cs
@@ -6,3 +6,7 @@
 Console.WriteLine("Hello, World!");  // Выводим Hello, world!
 Console.WriteLine("AbobA");          // Выводим абобу
 
+Console.WriteLine(args.FirstOrDefault());
+for (int i = 0; i < args.Length; i++) {
+    Console.Write("{0} ", args[i]);
+}
