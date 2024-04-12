6b72e2f4f2bb9676ab87ba0d562bbfad1c03b6b1
Darr Pamala <darrpama@un-h5.kzn.21-school.ru>
2024-04-12 21:26:57 +0300

    save

--- src/d00_ex00/Program.cs
+++ src/d00_ex00/Program.cs
@@ -1,50 +1,78 @@
 ﻿using System.Reflection.Metadata;
 
-if (args.Length != 3) {
+if (args.Length != 3)
+{
     throw new ArgumentException("Something went wrong. Check your input and retry.");
 }
 
-// for (int i = 0; i < args.Length; i++) {
+// for (int i = 0; i < args.Length; i++)
+// {
 //     Console.Write("{0} ", args[i]);
 // }
 
-double GetTotalMonthlyPayment(double loanAmmount, double interestRateOnLoanPerMounth, int mounths) {
-    if (mounths < 0) {
+double GetTotalMonthlyPayment(double loanAmmount, double interestRateOnLoanPerMounth, int mounths)
+{
+    if (mounths < 0)
+    {
         throw new ArgumentException("Number of mounths should be > 0");
     }
     double montlyPayment = 0;
-    montlyPayment = (loanAmmount*interestRateOnLoanPerMounth*Math.Pow((1+interestRateOnLoanPerMounth), mounths))/(Math.Pow((1+interestRateOnLoanPerMounth), mounths) - 1);
+    montlyPayment = (loanAmmount * interestRateOnLoanPerMounth * Math.Pow((1 + interestRateOnLoanPerMounth), mounths)) / (Math.Pow((1 + interestRateOnLoanPerMounth), mounths) - 1);
     return montlyPayment;
 }
 
-double GetInterestRateOnLoanPerMounth(double annualPercentageRate) {
+double GetInterestRateOnLoanPerMounth(double annualPercentageRate)
+{
     double interestRateOnLoanPerMounth = 0;
-    interestRateOnLoanPerMounth = annualPercentageRate/12/100;
+    interestRateOnLoanPerMounth = annualPercentageRate / 12 / 100;
     return interestRateOnLoanPerMounth;
 }
 
-double GetMonthlyPaymentInterest(double totalDebtBalance, double annualPercentageRate, int daysOfPeriod, int daysPerYear) {
+double GetMonthlyPaymentInterest(double totalDebtBalance, double annualPercentageRate, int daysOfPeriod, int daysPerYear)
+{
     double monthlyPaymentInterest = 0;
-    monthlyPaymentInterest = (totalDebtBalance*annualPercentageRate*daysOfPeriod)/(100*daysOfPeriod);
+    monthlyPaymentInterest = (totalDebtBalance * annualPercentageRate * daysOfPeriod) / (100 * daysPerYear);
     return monthlyPaymentInterest;
 }
 
-double LoanAmount = Convert.ToDouble(args[0]);
-double AnnualPercentageRate = Convert.ToDouble(args[1]);
-int NumberOfMonths = Convert.ToInt32(args[2]);
+double loanAmount = Convert.ToDouble(args[0]);
+double annualPercentageRate = Convert.ToDouble(args[1]);
+int numberOfMonths = Convert.ToInt32(args[2]);
 
 // Console.WriteLine("{0} ", LoanAmount);
 // Console.WriteLine("{0} ", AnnualPercentageRate);
 // Console.WriteLine("{0} ", NumberOfMonths);
 
-void PrintOutput(int mounths) {
+void PrintOutput(int mounths, double sum, double rate)
+{
     DateTime currentDate = DateTime.Now;
-    // var date = DateTime.ParseExact(dateStr, "MM-dd-yyyy", System.Globalization.CultureInfo.InvariantCulture);
-    for (int i = 1; i <= mounths; i++) {
-        // Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", i, date.ToString("MM/dd/yyyy"));
-        currentDate = currentDate.AddMonths(1);
-        Console.WriteLine("{0}\t{1}", i, currentDate.ToString("MM/dd/yyyy"));
+    DateTime paymentDate = currentDate.AddDays(-currentDate.Day + 1);
+    paymentDate = paymentDate.AddMonths(1);
+
+    double ratePerMonth = GetInterestRateOnLoanPerMounth(rate);
+    double monthlyPayment = GetTotalMonthlyPayment(sum, ratePerMonth, mounths);
+    double tmp = 0.0;
+    for (int i = 1; i <= mounths; i++)
+    {
+        int daysOfThePeriod = DateTime.DaysInMonth(paymentDate.Year, paymentDate.Month);
+        // int daysOfTheYear = DateTime.IsLeapYear(paymentDate.Year) ? 366 : 365;
+        int daysOfTheYear = 365;
+        paymentDate = paymentDate.AddMonths(1);
+        double monthlyPaymentInterest = GetMonthlyPaymentInterest(sum - tmp, rate, daysOfThePeriod, daysOfTheYear);
+        // Console.WriteLine("{0}\t{1}\t{2}\t{3}", sum, rate, daysOfThePeriod, daysOfTheYear);
+        double remainingDebt = sum - monthlyPayment;
+        double principalDebt = monthlyPayment - monthlyPaymentInterest;
+        if (remainingDebt < 0)
+        {
+            remainingDebt = 0;
+        }
+        Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", i, paymentDate.ToString("MM/dd/yyyy"), monthlyPayment.ToString("0.##"), ().ToString("0.##"), monthlyPaymentInterest.ToString("0.##"), remainingDebt.ToString("0.##"));
+        sum -= monthlyPayment;
+        tmp = monthlyPayment-monthlyPaymentInterest;
     }
 }
 
-PrintOutput(10);
+PrintOutput(numberOfMonths, loanAmount, annualPercentageRate);
+
+
+
