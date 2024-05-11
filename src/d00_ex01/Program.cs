c75dfec801f7260871b3fe615f88e66fc55d08ae
Darr Pamala <darrpama@si-c2.kzn.21-school.ru>
2024-05-11 23:53:48 +0300

    save

--- src/d00_ex01/Program.cs
+++ src/d00_ex01/Program.cs
@@ -1,7 +1,7 @@
-// int LevenshteinDistance(char s[0..m-1], char t[0..n-1]):
+
 int LevenshteinDistance(string s, string t) {
     // create two work vectors of integer distances
-    int n = t.Length + 1;
+    int n = t.Length;
     int m = s.Length;
     int[] v0 = new int[n + 1];
     int[] v1 = new int[n + 1];
@@ -9,7 +9,7 @@ int LevenshteinDistance(string s, string t) {
     // initialize v0 (the previous row of distances)
     // this row is A[0][i]: edit distance from an empty s to t;
     // that distance is the number of characters to append to  s to make t.
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n+1; i++) {
         v0[i] = i;
     }
 
@@ -18,10 +18,16 @@ int LevenshteinDistance(string s, string t) {
 
         // first element of v1 is A[i + 1][0]
         //   edit distance is delete (i + 1) chars from s to match empty t
+        foreach (int v in v0) {
+            Console.Write("[{0}]", v);
+        }
+        Console.Write("\n");
+
+
         v1[0] = i + 1;
 
         // use formula to fill in the rest of the row
-        for (int j = 0; j < n - 1; j++) {
+        for (int j = 0; j < n; j++) {
             // calculating costs for A[i + 1][j + 1]
             int deletionCost = v0[j + 1] + 1;
             int insertionCost = v1[j] + 1;
@@ -45,7 +51,7 @@ int LevenshteinDistance(string s, string t) {
 
         // copy v1 (current row) to v0 (previous row) for next iteration
         // since data in v1 is always invalidated, a swap without copy could be more efficient
-        for (int k = 0; k < n; k++) {
+        for (int k = 0; k < n + 1; k++) {
             v0[k] = v1[k];
         }
     }
